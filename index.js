fe51b4d7af886030accad49076cadc1b8d93d7a1
reinvantveer <reinvantveer@gmail.com>
2016-05-08 20:36:53 +0200

    Code style

--- index.js
+++ index.js
@@ -5,20 +5,23 @@ const FeatureBuilder = require('./lib/featureBuilder');
 const featureBuilder = new FeatureBuilder;
 
 const argv = require('yargs')
-    .usage('Usage: index.js --board [Eagle 7 board] --schema [Eagle 7 schema file] --output [output json file]')
-    .demand(['brd','sch', 'output'])
-    .alias('brd', 'board')
-    .alias('sch', 'schema')
-    .alias('o', 'output')
-    .argv;
+  .usage('Usage: index.js ' +
+    '--board [Eagle 7 board] ' +
+    '--schema [Eagle 7 schema file] ' +
+    '--output [output json file]')
+  .demand(['brd','sch', 'output'])
+  .alias('brd', 'board')
+  .alias('sch', 'schema')
+  .alias('o', 'output')
+  .argv;
 
 
 const boardXML = fs.readFileSync(argv.board, 'utf-8');
-const schemaXML = fs.readFileSync(argv.schema, 'utf-8');
+// const schemaXML = fs.readFileSync(argv.schema, 'utf-8');
 
 
 parseString(boardXML, (err, boardJS) => {
-  if (err) throw (err);
+  if (err) throw err;
   const boardGeoJSON = {
     type: 'FeatureCollection',
     features: []
@@ -31,6 +34,6 @@ parseString(boardXML, (err, boardJS) => {
   boardJS.eagle.drawing[0].board[0].signals[0].signal.forEach(
       signal => boardGeoJSON.features.push(featureBuilder.signal2feature(signal))
   );
-  
+
   fs.writeFileSync(argv.output, JSON.stringify(boardGeoJSON, null, 2));
 });
